e0c7e47379fb012bd19ed28407d8d68b99e6fbc9
Oddinx <ghostingphamton@gmail.com>
2022-03-03 22:52:48 -0500

    searchedit4

--- src/pages/SearchPost.js
+++ src/pages/SearchPost.js
@@ -13,8 +13,8 @@ import algoliasearch from "algoliasearch/lite";
 
 const SearchPost = ({ data }) => {
   const searchClient = algoliasearch(
-    process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
-    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY
+    process.env.ALGOLIA_APP_ID,
+    process.env.ALGOLIA_SEARCH_KEY
   );
   return (
     <Layout className="virgomain" data={data}>
