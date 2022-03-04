eeb741e43a0bdba6f171d453e5ca8cab2426c2d2
Oddinx <ghostingphamton@gmail.com>
2022-03-04 00:08:10 -0500

    Update SearchPost.js

--- src/pages/SearchPost.js
+++ src/pages/SearchPost.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, Component } from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 
@@ -11,50 +11,51 @@ import Container from "react-bootstrap/Container";
 
 import algoliasearch from "algoliasearch/lite";
 
-const SearchPost = ({ data }) => {
-  return (
-    <Layout className="virgomain" data={data}>
-      <SEO
-        title={data.contentfulSiteInformation.siteName}
-        keywords={[
-          `RandomHGamer`,
+export default class SearchPost extends Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <Layout className="virgomain" data={data}>
+        <SEO
+          title={data.contentfulSiteInformation.siteName}
+          keywords={[
+            `RandomHGamer`,
 
-          `Manga`,
-          `VideoJuegos`,
+            `Manga`,
+            `VideoJuegos`,
 
-          `Anime`,
+            `Anime`,
 
-          `Otaku`,
-          `Eroge`,
-        ]}
-      />
+            `Otaku`,
+            `Eroge`,
+          ]}
+        />
 
-      <TopBackGround data={data.contentfulGeneralmedia} />
+        <TopBackGround data={data.contentfulGeneralmedia} />
 
-      <div class="gap-1"></div>
-      <div class="gap-1"></div>
-      <div class="gap-1"></div>
-      <Container>
-        <ul class="breadcrumbs">
-          <li>
-            <span>Search</span>
-          </li>
-        </ul>
-      </Container>
-      <div class="gap-1"></div>
+        <div class="gap-1"></div>
+        <div class="gap-1"></div>
+        <div class="gap-1"></div>
+        <Container>
+          <ul class="breadcrumbs">
+            <li>
+              <span>Search</span>
+            </li>
+          </ul>
+        </Container>
+        <div class="gap-1"></div>
 
-      <Search
-        indexName={process.env.ALGOLIA_INDEX_NAME}
-        searchClient={algoliasearch(
-          process.env.GATSBY_ALGOLIA_APP_ID,
-          process.env.GATSBY_ALGOLIA_SEARCH_KEY
-        )}
-      />
-    </Layout>
-  );
-};
-
-export default SearchPost;
+        <Search
+          indexName={process.env.ALGOLIA_INDEX_NAME}
+          searchClient={algoliasearch(
+            process.env.GATSBY_ALGOLIA_APP_ID,
+            process.env.GATSBY_ALGOLIA_SEARCH_KEY
+          )}
+        />
+      </Layout>
+    );
+  }
+}
 
 export const pageQuery = graphql`
   query {
